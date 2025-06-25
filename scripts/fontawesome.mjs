a8b8234a68e276b3a74d260913bc7048af2d71a9
0xarch <alanqa-ops@outlook.com>
2025-06-25 20:37:38 +0800

    throw error when required icon not found

--- scripts/fontawesome.mjs
+++ scripts/fontawesome.mjs
@@ -8,5 +8,8 @@ export function camilaze(str) {
 export function get_fa_brand(_id) {
   const id = camilaze(String(_id));
   let resource = fortAwesome[id];
+  if(!resource) {
+    throw new Error(`Error while generating page: Cannot find required icon <${_id}> in social_links`);
+  }
   return `<svg viewBox="0 0 ${resource.icon[0]} ${resource.icon[1]}"><path d="${resource.icon[4]}" fill="currentColor"></path></svg>`;
 }
