2f86779757f70e38e05fe070c6cc5151a2475aac
Soloev <alanqa-ops@outlook.com>
2025-11-18 13:59:10 +0800

    fix image ref bug

--- scripts/copy.mjs
+++ scripts/copy.mjs
@@ -19,7 +19,7 @@ export default function (ctx) {
         referencedImages.forEach(v => {
             if (!regExps.MATCH_WEB_URL.test(v)) {
                 let originPath = join(dirname(post.full_source), v);
-                let targetPath = join(dirname(post.path), v);
+                let targetPath = join(ctx.PUBLIC_DIRECTORY,post.relative_path, v);
                 // let fallbackTargetPath = join(post.path, v);
                 cp(originPath, targetPath, (err) => {
                     if (err) {
